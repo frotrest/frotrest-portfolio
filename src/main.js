b33e6249c05ca0ad20a8f3d0231af56dec5e6956
TVOROG5776 <romansydorko10@gmail.com>
2025-11-25 14:05:09 +0200

    update

--- src/main.js
+++ src/main.js
@@ -1,13 +1,45 @@
-const btnOpenBurgerMenu = document.querySelector(".solid_menu")
-const btnCloseBurgerMenu = document.querySelector(".icon-close-burger-menu")
-const burgerMenu = document.querySelector(".burger-menu")
+document.addEventListener('DOMContentLoaded', () => {
+  const btnOpenBurgerMenu = document.querySelector('.solid_menu');
+  const btnCloseBurgerMenu = document.querySelector('.icon-close-burger-menu');
+  const burgerMenu = document.querySelector('.burger-menu');
 
-btnOpenBurgerMenu.addEventListener("click", (event) => {
-    burgerMenu.classList.add("is-open")
-  })
-  
-  btnCloseBurgerMenu.addEventListener("click", (event) => {
-    burgerMenu.classList.remove("is-open")
-  })
+  if (btnOpenBurgerMenu && burgerMenu) {
+    btnOpenBurgerMenu.addEventListener('click', () => {
+      burgerMenu.classList.add('is-open');
+    });
+  }
 
-  
+  if (btnCloseBurgerMenu && burgerMenu) {
+    btnCloseBurgerMenu.addEventListener('click', () => {
+      burgerMenu.classList.remove('is-open');
+    });
+  }
+  try {
+    const animatedElements = document.querySelectorAll('[data-animate]');
+
+    const observer = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting && entry.target instanceof Element) {
+            const animation = entry.target.dataset.animate;
+            entry.target.classList.add(
+              'animate__animated',
+              `animate__${animation}`
+            );
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      {
+        root: null,
+        threshold: 0.1,
+      }
+    );
+
+    animatedElements.forEach(el => {
+      observer.observe(el);
+    });
+  } catch (error) {
+    console.log(`Ошибка с анимациями: ${error}`);
+  }
+});
